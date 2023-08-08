47c5e0f49f01dc3c3c3bb842a279d4cf694426fd
Deepak Fuloria <frontenddeveloperintern1@gmail.com>
2023-08-08 10:48:16 +0530

    m1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/userRoutes")
 const profileRoutes = require("./routes/profileRoutes")
 const postRoutes = require("./routes/postRoutes")
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 // DB Connnection
 connect();
 // Express session middleware
@@ -42,6 +42,8 @@ app.use(profileRoutes)
 app.use(postRoutes)
 
 // Create server
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port number: ${PORT}`);
 });
